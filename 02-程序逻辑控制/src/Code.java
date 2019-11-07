23f63a36c3f7cb5aeb680382e840caf3a5c9b525
KexinAnswer <925433202@qq.com>
2019-11-07 22:38:15 +0800

    for 输入 输出

---
+++ 02-程序逻辑控制/src/Code.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+        // 1. 根据年龄，打印出当前年龄的人是少年(低于18), 青年(19-28),
+        // 中年(29-55),老年(56以上)
+        // Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+        // if(age < 18){
+        //     System.out.println("少年");
+        // }else if(age < 28){
+        //     System.out.println("青年");
+        // }else if(age < 55){
+        //     System.out.println("中年");
+        // }else {
+        //     System.out.println("老年");
+        // }
+
+        // 2. 判定一个数字是否是素数
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // int i = 0;
+        // for (i = 2; i < Math.sqrt(num) ; i++) {
+        //     if(num % i == 0){
+        //         System.out.println("不是素数");
+        //         break;
+        //     }
+        // }
+        // if(i > Math.sqrt(num)){
+        //     System.out.println("是素数");
+        // }
+
+        // 3. 打印 1-100 之间所有的素数
+
+    w}
+}
--- 02-程序逻辑控制/src/logic.java
+++ 02-程序逻辑控制/src/logic.java
@@ -1,3 +1,8 @@
+import com.sun.org.apache.xerces.internal.impl.xs.SchemaNamespaceSupport;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class logic {
     public static void main(String[] args) {
         // 1. 顺序结构
@@ -66,19 +71,103 @@ public class logic {
         // System.out.println(res);
 
 
-        int n = 1;
-        int sum = 0;
-        while(n <= 5){
-            int res = 1;
-            int i = 1;
-            while(i <= n){
-                res *= i;
-                ++i;
+        // int n = 1;
+        // int sum = 0;
+        // while(n <= 5){
+        //     int res = 1;
+        //     int i = 1;
+        //     while(i <= n){
+        //         res *= i;
+        //         ++i;
+        //     }
+        //     ++n;
+        //     System.out.println(res);
+        //     sum += res;
+        // }
+        // System.out.println(sum);
+
+        // 3.2 break
+        // 找到 100 - 200 中第一个三的倍数
+        // int num = 100;
+        // while(num < 300){
+        //     if(num % 3 == 0){
+        //         break;
+        //     }
+        //     num++;
+        // }
+
+        // System.out.println(num);
+
+        // 3.3 continue
+        // 找到 100-200 中所有 3 的倍数
+
+         // 找到 100 - 200 中第一个三的倍数
+         //int num = 100;
+         //while(num < 300){
+         //    if(num % 3 != 0){
+         //        num++;
+         //        continue;
+         //    }
+         //    num++;
+         //    System.out.println(num);
+         //}
+
+        // 3.4 for
+        //for (int i = 1; i <=10 ; i++) {
+        //    System.out.println(i);
+        //}
+
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     ++i;
+        // }while(i <= 10);
+
+        // 4.1 输入到控制台
+        // String msg = "hello world";
+        // System.out.println(msg);
+        // System.out.print(msg);
+        // System.out.printf("%s\n", msg);
+
+        // 4.2 从键盘输入
+        // Scanner sc = new Scanner(System.in);
+        // System.out.printf("请输入姓名:");
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // System.out.println("请输入年龄:");
+        // int age = sc.nextInt();
+        // System.out.println(age);
+        // sc.close();
+
+        // 使用 Scanner 循环读取 N 个数字
+        // Scanner sc = new Scanner(System.in);
+        // double sum = 0;
+        // int num = 0;
+        // while(sc.hasNextDouble()){
+        //     double tmp = sc.nextDouble();
+        //     sum += tmp;
+        //     num++;
+        // }
+        // System.out.println(sum);
+
+        // 5 猜数字游戏
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int toGuess = random.nextInt(100);
+        while(true){
+            System.out.println("请输入1-100以内的数字");
+            int num = sc.nextInt();
+            if(num > toGuess){
+                System.out.println("猜大了");
+            }else if(num < toguess){
+                System.out.println("猜小了");
+            }else{
+                System.out.println("猜对了");
+                System.out.println("答案是" + toGuess);
+                break;
             }
-            ++n;
-            System.out.println(res);
-            sum += res;
         }
-        System.out.println(sum);
     }
 }
+
